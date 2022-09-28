562a8f8d74ed553154e3a52845ac2e1fa1e353fe
yangshindo <eliwood182@hotmail.com>
2022-09-28 12:31:34 -0300

    tournament box 2

--- src/Tournament.js
+++ src/Tournament.js
@@ -6,26 +6,27 @@ function Tournament() {
       <Box
         sx={{
           border: "solid",
-          borderRadius: "5%",
+          borderRadius: "8%",
           padding: "5%",
-          backgroundColor: "primary.dark",
+          backgroundColor: "#950101",
           "&:hover": {
-            backgroundColor: "primary.main",
+            backgroundColor: "#950101",
             opacity: [0.9, 0.8, 0.7],
           },
         }}
       >
         <Grid container spacing={10}>
-          <Grid item xs={6}>
+          <Grid item xs={5.5}>
+            <h2 className="tournament-h2">2022 WORLD TOURNAMENT</h2>
             <p className="tournament-text">
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
               reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id."
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident."
             </p>
+            <hr className="tournament-hr" />
           </Grid>
           <Grid item xs={6}>
             <img
